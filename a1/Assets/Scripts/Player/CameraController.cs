aae159b5949f97f0576e0a47ab7bf9853b15a7ca
garanschulz <luigipeg12345@gmail.com>
2024-02-08 15:51:13 -0700

    Basic camera raycast
    Very minor progress on adding camera features, taking a break because it took a while

--- a1/Assets/Scripts/Player/CameraController.cs
+++ a1/Assets/Scripts/Player/CameraController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class CameraController : MonoBehaviour
 {
+    #region UnchangedStuff
     private Vector3 cameraFollowVelocity = Vector3.zero;
     [SerializeField]
     Transform targetTransform;
@@ -19,7 +22,14 @@ public class CameraController : MonoBehaviour
     float maxPivotAngle = 35;
     [SerializeField]
     Transform cameraPivot;
+    #endregion
 
+    [SerializeField]
+    private float cameraSmoothSpeed = 0.5f;
+    [SerializeField]
+    LayerMask groundLayer;
+    [SerializeField]
+    LayerMask wallLayer;
 
     private float lookAngle = 0, pivotAngle = 0;
     void Awake()
@@ -54,5 +64,26 @@ public class CameraController : MonoBehaviour
     private void LateUpdate()
     {
         HandleAllCameraMovement();
+        CameraRaycast();
+    }
+
+    private void CameraSmoothing(float alpha)
+    {
+        alpha = cameraSmoothSpeed;
+
+    }
+
+    private void CameraRaycast()
+    {
+        //RaycastHit hit;
+        Vector3 cameraPos = GetComponentInChildren<Camera>().transform.position;
+        Vector3 playerPos = targetTransform.position;
+        Vector3 direction = playerPos - cameraPos;
+        Debug.DrawRay(cameraPos, direction, Color.blue, 1.0f);
+        if (Physics.Raycast(cameraPos, direction, transform.forward.magnitude, wallLayer))
+        //if (Physics.SphereCast(cameraPos, 0.5f, direction, out hit, wallLayer))
+        {
+            Debug.Log("WE ARE IN THE BEAM");
+        }
     }
 }
