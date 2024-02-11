265dce5db737f6e9ca5b559adf2a3d0749f157c4
Leo Esguerra <leo.esguerra@gmail.com>
2024-02-11 11:08:37 -0700

    Add zoom when target is blocked

--- a1/Assets/Scripts/Player/CameraController.cs
+++ a1/Assets/Scripts/Player/CameraController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -14,18 +15,30 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private float cameraPivotSpeed = 2f;
     [SerializeField]
+    private float cameraZoomSpeed = 0.2f;
+    [SerializeField]
     float minPivotAngle = -35;
     [SerializeField]
     float maxPivotAngle = 35;
     [SerializeField]
     Transform cameraPivot;
-
-
+    [SerializeField]
+    private bool _showDebugRays = true;
     private float lookAngle = 0, pivotAngle = 0;
+    private Vector3 targetPositionOffset;
+    private float cameraMaxDistance;
     void Awake()
     {
         targetTransform = FindObjectOfType<PlayerController>().transform;
-        //camera = GetComponentInChildren<Camera>();
+        
+    }
+    private void Start()
+    {
+        GameObject player = FindObjectOfType<PlayerController>().gameObject;
+        CapsuleCollider collider = player.GetComponent<CapsuleCollider>();
+
+        //Use offset to target player midsection instead of feet
+        targetPositionOffset = new Vector3(0, collider.height * 0.5f, 0);
     }
     private void HandleAllCameraMovement()
     {
@@ -33,8 +46,32 @@ public class CameraController : MonoBehaviour
     }
     private void FollowTarget()
     {
-        Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
+        Vector3 adjustTargetPosition = targetTransform.position + targetPositionOffset;
+        
+        //float distance = Vector3.Distance(adjustTargetPosition, Camera.main.transform.position);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(adjustTargetPosition);
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+        RaycastHit hitInfo;
+
+        Vector3 targetPosition = targetTransform.position;
+        Color debugRayColor = Color.green;
+        float cameraMoveSpeed = cameraFollowSpeed;
+        cameraMaxDistance = 3.5f;
+        
+        if(Physics.Raycast(adjustTargetPosition, -ray.direction, out hitInfo, cameraMaxDistance))
+        {
+            targetPosition = adjustTargetPosition + (ray.direction.normalized * (cameraMaxDistance - hitInfo.distance));
+            cameraFollowSpeed = cameraZoomSpeed;
+            debugRayColor = Color.red;
+        }
+
+        targetPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraFollowVelocity, cameraMoveSpeed);
         transform.position = targetPosition;
+
+        if(_showDebugRays)
+        {
+            Debug.DrawRay(adjustTargetPosition, -ray.direction * cameraMaxDistance, debugRayColor);
+        }
     }
 
     public void RotateCamera(Vector2 movement)
