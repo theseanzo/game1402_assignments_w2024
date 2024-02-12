24e9402a1d421c54e9e04c122507067c7e7e11a4
yasubyigit <yigityasub1@gmail.com>
2024-02-11 22:24:53 -0700

    Except animations all the tasks are done.

--- a1/Assets/Scripts/Foods/Food.cs
+++ a1/Assets/Scripts/Foods/Food.cs
@@ -1,39 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    bool hit = false;
+    [SerializeField]
+    private float durationBeforeRespawn = 10f; // Duratioin before the collectible is going to respawn again.
+    [SerializeField]
+    private float appearTransitionTime = 5f; // Duration of the effect for reappear
     public int Value
     {
         get; protected set;
     }
-    void Awake()
+    private bool isCollected = false;
+    public int PickupValue { get; private set; }
+
+    private void Awake()
     {
-        Collider foodCollider = GetComponent<Collider>();
-        foodCollider.isTrigger = true;
-        Value = GameConstants.BaseFoodValue;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        var itemCollider = GetComponent<Collider>();
+        itemCollider.isTrigger = true;
+        PickupValue = GameConstants.BaseFoodValue; 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<PlayerController>() && !hit)
+        if (other.gameObject.GetComponent<PlayerController>() && !isCollected)
         {
-            hit = true;
-            GameManager.Instance.Score += Value; //recall that the value is set in each one of the food's children
-            Destroy(this.gameObject);
+            isCollected = true;
+            GameManager.Instance.Score += PickupValue;
+            StartCoroutine(ReappearAfterDelay());
         }
-        
+    }
+
+    IEnumerator ReappearAfterDelay()
+    {
+        SetItemVisibility(false);
+
+        yield return new WaitForSeconds(durationBeforeRespawn);
+
+        float timePassed = 0f;
+        while (timePassed < appearTransitionTime)
+        {
+            timePassed += Time.deltaTime;
+            float blend = timePassed / appearTransitionTime;
+            AdjustItemTransparency(blend);
+            yield return null;
+        }
+
+        SetItemVisibility(true);
+        isCollected = false;
+    }
+
+    private void SetItemVisibility(bool isVisible)
+    {
+        var itemRenderer = GetComponent<Renderer>();
+        itemRenderer.enabled = isVisible;
+        GetComponent<Collider>().enabled = isVisible;
+        if (isVisible) // Reset transparency when becoming visible
+        {
+            itemRenderer.material.color = new Color(itemRenderer.material.color.r, itemRenderer.material.color.g, itemRenderer.material.color.b, 1f);
+        }
+    }
+
+    private void AdjustItemTransparency(float alpha)
+    {
+        var renderer = GetComponent<Renderer>();
+        renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, alpha);
     }
 }
