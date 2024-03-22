94a93017d4facea99b16c29122f78574ce14fcf4
Suellen S <suellendesalles@gmail.com>
2024-03-22 13:55:41 -0600

    Overriding ShiftSheep.Move method to move sheep smoothly. Adding coroutine to move smoothly the sheep position based on distance.

--- a2/Assets/Scripts/A2_Scripts/ShiftSheep.cs
+++ a2/Assets/Scripts/A2_Scripts/ShiftSheep.cs
@@ -8,11 +8,31 @@ public class ShiftSheep : A2Animal
     [SerializeField]
     float distance = 2f;
     #endregion
+    IEnumerator coroutine;
+    Vector3 newPosition;
+
     private void Update()
     {
         #region Sean code do not touch
         if (Input.GetKeyDown(KeyCode.Alpha3))
             Move();
         #endregion
+    }
+
+    protected override void Move()
+    {
+        newPosition = new Vector3(_rb.position.x + distance, _rb.position.y, _rb.position.z);
+        coroutine = MoveSheep();
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator MoveSheep()
+    {
+        while (true)
+        {
+            float interpolation = .5f;
+            _rb.position = Vector3.Lerp(_rb.position, newPosition, interpolation * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
