d6d8578f3d9a7ad3995c400833cc0994baec894a
Suellen S <suellendesalles@gmail.com>
2024-03-22 12:25:39 -0600

    Overriding ConstantMoveGoat.Move method to ensure that the goat is moving or not based on each key press. Adding coroutine to move the goat position.

--- a2/Assets/Scripts/A2_Scripts/ConstantMoveGoat.cs
+++ a2/Assets/Scripts/A2_Scripts/ConstantMoveGoat.cs
@@ -8,11 +8,39 @@ public class ConstantMoveGoat : A2Animal
     [SerializeField]
     float speed = 3f;
     #endregion
+    bool isMoving;
+
+    IEnumerator couroutine;
+
     private void Update()
     {
         #region SEAN CODE DO NOT TOUCH
         if (Input.GetKeyDown(KeyCode.Alpha1))
             Move(); //can change move functions
         #endregion
+    }
+
+    protected override void Move()
+    {
+        if (!isMoving)
+        {
+            isMoving = true;
+            couroutine = MoveGoat();
+            StartCoroutine(couroutine);
+        }
+        else
+        {
+            isMoving = false;
+            StopCoroutine(couroutine);
+        }
+    }
+
+    IEnumerator MoveGoat()
+    {
+        while (true)
+        {
+            _rb?.MovePosition(_rb.position + Vector3.right * speed * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
