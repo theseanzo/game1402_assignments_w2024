cb866723476c6a5be392469ced96e19fa5bf60fb
Suellen S <suellendesalles@gmail.com>
2024-03-22 12:17:41 -0600

    Removing temporary rb movement code. Setting _rb variable to protected to access it in child classes. Removing unnecessary "this" and comment.

--- a2/Assets/Scripts/A2_Scripts/A2Animal.cs
+++ a2/Assets/Scripts/A2_Scripts/A2Animal.cs
@@ -3,27 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class A2Animal : MonoBehaviour
 {
-    Rigidbody _rb;
+    protected Rigidbody _rb;
 
     private void Awake()
     {
-        _rb = this.gameObject.GetComponent<Rigidbody>();
+        _rb = gameObject.GetComponent<Rigidbody>();
         if (!_rb)
-            _rb = this.gameObject.AddComponent<Rigidbody>();
+            _rb = gameObject.AddComponent<Rigidbody>();
         _rb.freezeRotation = true;
         _rb.isKinematic = true;
     }
 
-    // Update is called once per frame
-
     protected virtual void Move()
     {
-        
+
     }
 
-    private void FixedUpdate()
-    {
-        if (_rb)
-            _rb?.MovePosition(_rb.position + Vector3.right * 3f * Time.fixedDeltaTime); //set temporary movement
-    }
 }
