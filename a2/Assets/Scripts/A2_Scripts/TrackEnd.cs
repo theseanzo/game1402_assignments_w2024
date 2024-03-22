76c3f7dc451dceae52f39b9680c5258aa06a9ef1
Suellen S <suellendesalles@gmail.com>
2024-03-22 10:35:31 -0600

    Setting TrackEnd MeshCollider: - enabling MeshCollider; - enabling convex; - enabling isTrigger. Destroying animal when it collides with the TrackEnd and triggers it.

--- a2/Assets/Scripts/A2_Scripts/TrackEnd.cs
+++ a2/Assets/Scripts/A2_Scripts/TrackEnd.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class TrackEnd : MonoBehaviour
 {
+    private void Awake()
+    {
+        MeshCollider _meshCollider = gameObject.GetComponent<MeshCollider>();
+        _meshCollider.enabled = true;
+        _meshCollider.convex = true;
+        _meshCollider.isTrigger = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,10 @@ public class TrackEnd : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
