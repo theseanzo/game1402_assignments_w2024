16788394e5e2be3f14aef590e544aef711894789
Leo Esguerra <leo.esguerra@gmail.com>
2024-03-22 18:35:32 -0600

    Handle despawn on TrackSpawner too

--- a2/Assets/Scripts/A2_Scripts/TrackSpawner.cs
+++ a2/Assets/Scripts/A2_Scripts/TrackSpawner.cs
@@ -15,6 +15,36 @@ public class TrackSpawner : MonoBehaviour
 	//NOTE: Every A2 Animal, when spawned, will need to be rotated 90 on the y axis
 	public void Spawn() 
 	{
-		
+		// Spawn an animal at the spawn location
+		// Set the animal's onReachTrackEndDelegate to Despawn
+		currentAnimal = Instantiate(animal, spawnLocation.position, Quaternion.Euler(0, 90, 0), this.transform);
+		currentAnimal.onReachTrackEndDelegate += Despawn;
+	}
+
+	private void Update()
+	{
+		if(currentAnimal != null)
+		{
+			IsAnimalAtTrackEnd();
+		}
+	}
+
+	public void IsAnimalAtTrackEnd()
+	{
+		if(currentAnimal.transform.position.x >= endLocation.position.x)
+		{
+			Despawn();
+		}
+	}
+
+	public bool CanSpawn()
+	{
+		return currentAnimal == null;
+	}
+
+	private void Despawn()
+	{
+		Destroy(currentAnimal.gameObject);
+		currentAnimal = null;
 	}
 }
