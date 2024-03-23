1944d393eb8940c5f2d7305f1ddc4b24d233fd89
Leo Esguerra <leo.esguerra@gmail.com>
2024-03-22 18:34:39 -0600

    Call spawn on trackSpawner if it can spawn

--- a2/Assets/Scripts/A2_Scripts/Exhibit3_A2.cs
+++ a2/Assets/Scripts/A2_Scripts/Exhibit3_A2.cs
@@ -17,8 +17,15 @@ public class Exhibit3_A2 : Exhibit
         #endregion
     }
 
+	// Loop through all the track spawners and spawn an animal if it can
     public void StartSpawning()
 	{
-
+		foreach(TrackSpawner trackSpawner in trackSpawners)
+		{
+			if(trackSpawner.CanSpawn())
+			{
+				trackSpawner.Spawn();
+			}
+		}
 	}
 }
