78a34e76176977c4367d7a6d398f545b775f20fd
Leo Esguerra <leo.esguerra@gmail.com>
2024-03-22 20:12:37 -0600

    Handle ShiftSheep movement

--- a2/Assets/Scripts/A2_Scripts/ShiftSheep.cs
+++ a2/Assets/Scripts/A2_Scripts/ShiftSheep.cs
@@ -8,11 +8,42 @@ public class ShiftSheep : A2Animal
     [SerializeField]
     float distance = 2f;
     #endregion
+
+    private float _duration = 0.5f;
+    private Coroutine _shiftMoveCoroutine;
     private void Update()
     {
         #region Sean code do not touch
         if (Input.GetKeyDown(KeyCode.Alpha3))
             Move();
         #endregion
+    }
+
+    // Start a coroutine to move the animal
+    // If the coroutine is already running, do nothing
+    protected override void Move()
+    {
+        if (_shiftMoveCoroutine == null)
+        {
+            _shiftMoveCoroutine = StartCoroutine(ShiftMove());
+        }
+    }
+
+    // Move the animal to the right by a set distance smoothly over a set duration
+    IEnumerator ShiftMove()
+    {
+        float time = 0;
+        Vector3 positionStart = transform.localPosition;
+        Vector3 positionTarget = positionStart + Vector3.right * distance;
+
+        while (time < _duration)
+        {
+            time += Time.deltaTime;
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, positionTarget, distance / _duration * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.localPosition = positionTarget;
+        _shiftMoveCoroutine = null;
     }
 }
