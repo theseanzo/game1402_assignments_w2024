60fb2b76aece9f850c23e89a4d397a609fe0df53
Leo Esguerra <leo.esguerra@gmail.com>
2024-03-22 18:36:34 -0600

    Handle ConstantGoat movement

--- a2/Assets/Scripts/A2_Scripts/ConstantMoveGoat.cs
+++ a2/Assets/Scripts/A2_Scripts/ConstantMoveGoat.cs
@@ -8,11 +8,34 @@ public class ConstantMoveGoat : A2Animal
     [SerializeField]
     float speed = 3f;
     #endregion
+
+    private bool _shouldMove = false;
     private void Update()
     {
         #region SEAN CODE DO NOT TOUCH
         if (Input.GetKeyDown(KeyCode.Alpha1))
             Move(); //can change move functions
         #endregion
+
+        if(_shouldMove)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
     }
+
+    protected override void Move()
+    {
+        _shouldMove = !_shouldMove;
+    }
+
+    protected override void AddRigidBody()
+    {
+        base.AddRigidBody();
+    }
+
+	protected override void ModifyRigidBody()
+	{
+        base.ModifyRigidBody();
+		_rb.isKinematic = true;
+	}
 }
