239d3cf8dfe72d15993150a725d456bf9001999f
Samsayed786 <samsayed07866@gmail.com>
2024-03-22 23:40:01 -0600

    Revert "sayed akid hussain"
    This reverts commit 43b25e2c2cc043c5bb8ffbd609a6435b0e11fdb0.

--- a2/Assets/Scripts/A2_Scripts/TrackSpawner.cs
+++ a2/Assets/Scripts/A2_Scripts/TrackSpawner.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,39 +6,15 @@ public class TrackSpawner : MonoBehaviour
 {
     #region Sean Code Do Not Touch
     [SerializeField]
-    public Transform spawnLocation, endLocation;
-    [SerializeField]
-    A2Animal animal;
-    A2Animal currentAnimal;
-
-    private float destroyThreshold = 1f;
-
+	Transform spawnLocation, endLocation;
+	[SerializeField]
+	A2Animal animal;
+	A2Animal currentAnimal;
     #endregion
 
-
-    public void Update()
-    {
-        if (currentAnimal != null)
-        {
-            float distanceToEndpoint = Vector3.Distance(currentAnimal.transform.position, endLocation.position);
-
-            if (distanceToEndpoint <= destroyThreshold)
-            {
-                Destroy(currentAnimal.gameObject);
-                currentAnimal = null;
-            }
-        }
-    }
-
-        //NOTE: Every A2 Animal, when spawned, will need to be rotated 90 on the y axis
-        public void Spawn()
-        {
-            if (currentAnimal == null && animal != null && spawnLocation != null && endLocation != null)
-            {
-                currentAnimal = Instantiate(animal, spawnLocation.position, Quaternion.identity);
-                currentAnimal.transform.LookAt(endLocation);
-            }
-        }
-    
-
+	//NOTE: Every A2 Animal, when spawned, will need to be rotated 90 on the y axis
+	public void Spawn() 
+	{
+		
+	}
 }
