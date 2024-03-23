1e3f53941a9393cd2948599498eeaa5176441b7e
Leo Esguerra <leo.esguerra@gmail.com>
2024-03-22 18:36:06 -0600

    Setup default rigidbody and allow modification on child objects

--- a2/Assets/Scripts/A2_Scripts/A2Animal.cs
+++ a2/Assets/Scripts/A2_Scripts/A2Animal.cs
@@ -4,11 +4,59 @@ using UnityEngine;
 public class A2Animal : MonoBehaviour
 {
 
+    protected Rigidbody _rb;
+    public delegate void OnReachTrackEndDelegate();
+    public OnReachTrackEndDelegate onReachTrackEndDelegate;
     // Update is called once per frame
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        AddRigidBody();
+    }
 
     protected virtual void Move()
     {
         
     }
 
+    private bool HasRigidBody()
+    {
+        return _rb != null;   
+    }
+
+    // Add a rigid body to the game object if it doesn't have one
+    protected virtual void AddRigidBody()
+    {
+        if (!HasRigidBody())
+        {
+            _rb = gameObject.AddComponent<Rigidbody>();
+            _rb.mass = 1;
+            _rb.drag = 0;
+            _rb.angularDrag = 0.05f;
+            _rb.useGravity = true;
+            _rb.isKinematic = false;
+
+            _rb.interpolation = RigidbodyInterpolation.None;
+            _rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
+            _rb.freezeRotation = true;    
+        }
+
+        ModifyRigidBody();
+    }
+
+    protected virtual void ModifyRigidBody()
+    {
+
+    }
+
+    // Check if the animal has reached the end of the track
+    // Call the delegate if it has
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "TrackEnd")
+        {
+            onReachTrackEndDelegate();
+        }
+    }
 }
